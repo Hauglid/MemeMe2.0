142b3b9bb62914c95cc28874f7a108e894c3a378
Hauglid, Thomas <karl.thomas.hauglid@storebrand.no>
2019-01-12 19:18:41 +0100

    Outlined text on labels in collectionCell

--- MemeMe/Views/MemeCollectionViewCell.swift
+++ MemeMe/Views/MemeCollectionViewCell.swift
@@ -14,6 +14,16 @@ class MemeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var topText: UILabel!
     @IBOutlet weak var bottomText: UILabel!
 
-    
+    func setCellContent(text: String) {
+        let strokeTextAttributes: [NSAttributedString.Key : Any] = [
+            .strokeColor : UIColor.black,
+            .foregroundColor : UIColor.white,
+            .strokeWidth : -2.0,
+            ]
+        
+        topText.attributedText = NSAttributedString(string: "Foo", attributes: strokeTextAttributes)
+        bottomText.attributedText = NSAttributedString(string: "Foo", attributes: strokeTextAttributes)      
+        
+    }
 }
 
